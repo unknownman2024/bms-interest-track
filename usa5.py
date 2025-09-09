93263ed06e7c854093667cce283038e4074f99cb
unknownman2024 <text2024mail@gmail.com>
2025-09-10 00:58:29 +0530

    Update usa5.py

--- usa5.py
+++ usa5.py
@@ -94,7 +94,7 @@ def get_random_ip():
 def get_seatmap_headers():
     random_ip = get_random_ip()
     return {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent": get_random_user_agent(),
         "Origin": "https://fandango.com",
         "Referer": "https://tickets.fandango.com/mobileexpress/seatselection",
         "Connection": "keep-alive",
@@ -103,8 +103,6 @@ def get_seatmap_headers():
         "authority": "tickets.fandango.com",
         "accept": "application/json",
     }
-
-
 # === Helper functions for language and format extraction ===
 
 
