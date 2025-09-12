ebbaeee09242d5b9ee2ce87f6177054514d49f4e
unknownman2024 <text2024mail@gmail.com>
2025-09-12 13:44:20 +0530

    Update aus_hoyts.py

--- aus_hoyts.py
+++ aus_hoyts.py
@@ -24,11 +24,41 @@ def get_random_ip():
 
 def get_headers():
     return {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1",
-        "Referer": "https://www.hoyts.com.au/",
-        "Origin": "https://www.hoyts.com.au",
-        "X-Forwarded-For": get_random_ip(),
+        "authority": "apim.hoyts.com.au",
+        "method": "GET",
+        "path": "/au/cinemaapi/api/movies/",
+        "scheme": "https",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "cache-control": "no-cache",
+        "cookie": (
+            "_ga=GA1.1.1371980955.1748592304; "
+            "_scid=_Cp6fVJd9sTo5SbNZmAI9vg_NvyPxGsY; "
+            "_hjSessionUser_1508041=eyJpZCI6IjYyOTg5MTI1LTczYTItNTA4Zi1hNTA4LWFhMmFmNDg1YzdlZCIsImNyZWF0ZWQiOjE3NDg1OTIzMDQxMDQsImV4aXN0aW5nIjp0cnVlfQ==; "
+            "_tt_enable_cookie=1; "
+            "_ttp=01K1R0Z4Z02G2ZY21ABPHDVNFR_.tt.2; "
+            "ttcsid_C2385GI4I5JO5S1JBV30=1754226725858::apviTUsGDJw_vVsWOLD3.1.1754226890277; "
+            "ttcsid=1754226725859::-YsP_k4K0ZnbuQjQvuu7.1.1754226890277; "
+            "_gcl_au=1.1.755051459.1756878200; "
+            "_ScCbts=%5B%5D; "
+            "_sctr=1%7C1757529000000; "
+            "_scid_r=Bap6fVJd9sTo5SbNZmAI9vg_NvyPxGsYIVgVRA; "
+            "_ga_SQXDT3PQ8J=GS2.1.s1757593458$o6$g1$t1757594821$j60$l0$h0; "
+            "_cfuvid=DJbtG01tzQmKdZ2dX92eBf_IfzLtI1WdryLX4hVspRI-1757664049635-0.0.1.1-604800000"
+        ),
+        "dnt": "1",
+        "pragma": "no-cache",
+        "priority": "u=0, i",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1",
+        "x-forwarded-for": get_random_ip(),
     }
+
 
 scraper = cloudscraper.create_scraper()
 
