ce55385c556994501e39691b1309aee49f45042e
unknownman2024 <text2024mail@gmail.com>
2026-05-28 18:12:05 +0530

    Update canada1.py

--- canada1.py
+++ canada1.py
@@ -14,7 +14,7 @@ CINEPLEX_FILMS = {
 }
 
 
-TARGET_START="2026-06-04"
+TARGET_START="2026-06-03"
 
 today=datetime.now(ZoneInfo("America/Toronto")).date()
 target=datetime.fromisoformat(TARGET_START).date()
